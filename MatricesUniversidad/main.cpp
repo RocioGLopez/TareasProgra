f90bf824fc7cb1c655009aa7004c177bc0a87637
Rocio Lopez <guisell.alvarado@gmail.com>
2024-04-08 20:54:52 -0600

    Programa de matrices universidad, uso de bibliotecas

---
+++ MatricesUniversidad/main.cpp
@@ -0,0 +1,14 @@
+// Ejemplo de utilización de matrices codigo heredado de C
+//Declaraciòn de Bibliotecas y librerias estandar de C++
+#include <iostream>
+#include "Matrices.h"
+
+using namespace std;
+
+int main()
+{
+
+    srand(getpid()); //Proceso preparativo para generaciòn de valores aleatorios
+    llamaCiclo();
+    return 0;
+}
